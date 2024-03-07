987aef4f0cfecd0b45867b213899b7569fc6a612
Allan-Quiterio <allanquiterio100@gmail.com>
2024-03-07 10:32:16 -0300

    feat: visão geral das Generics

--- src/Generics/script.js
+++ src/Generics/script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Generics");
+function generics() {
+    function myFilter(array, callbackfn) {
+        const newArray = [];
+        for (let i = 0; i < array.length; i++) {
+            if (callbackfn(array[i])) {
+                newArray.push(array[i]);
+            }
+        }
+        return newArray;
+    }
+    const array = [1, 2, 3, 4, 5, 6, 7, 8];
+    const menoresCinco = array.filter((value) => value < 5);
+    console.log(menoresCinco);
+    const arrayFiltrado = myFilter(array, (value) => value < 5);
+    console.log(arrayFiltrado);
+}
+generics();
--- src/Generics/script.ts
+++ src/Generics/script.ts
@@ -1,1 +1,25 @@
-console.log("Generics");
+function generics() {
+  type FilterCallback<T> = (value: T, index?: number, array?: T[]) => boolean;
+
+  //Criando um tipo genérico e passando ele para um type, que também foi criado um tipo genérico a cima.
+  function myFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+      if (callbackfn(array[i])) {
+        newArray.push(array[i]);
+      }
+    }
+    return newArray;
+  }
+
+  const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const menoresCinco = array.filter((value) => value < 5);
+  console.log(menoresCinco);
+
+  const arrayFiltrado = myFilter(array, (value) => value < 5);
+  console.log(arrayFiltrado);
+}
+
+generics();
